08b693cad4b5e75568cff5ba308dfe9737793783
Mason Wiley <masonwiley92@gmail.com>
2015-09-04 10:32:07 -0700

    fixes assets not being added to Sprockets

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,3 +1,16 @@
 require "jekyll-assets"
-require "jekyll-assets/bootstrap"
-require "jekyll-assets/rails-assets"
+require "sprockets"
+
+gemspec = Gem::Specification.find_by_name "bootstrap-sass"
+
+%w(images fonts javascripts stylesheets).each do |asset|
+  Sprockets.append_path File.join(gemspec.gem_dir, "assets", asset)
+end
+
+Bundler.require(:rails_assets)
+
+if defined? RailsAssets
+  RailsAssets.components.flat_map(&:load_paths).each do |path|
+    Sprockets.append_path(path)
+  end
+end
